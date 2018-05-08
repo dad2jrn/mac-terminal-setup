f44a3178a2abb9bc7551593ed3226aadbb8e9c92
Dad 2 JRN <dad2jrn@gmail.com>
2018-05-08 13:45:10 -0400

    check for iterm2 install

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 # Check if Homebrew is installed
 if ! type "brew" > /dev/null; then
   # install foobar here
+  echo -e '\nInstalling the MacOS Package Manager, Homebrew...\n'
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# check if iTerm2 is installed
+if [ ! -d "/Applications/iTerm.app/Contents/MacOS/iTerm2" ]; then
+  # Install iTerm2
+  echo -e '\nInstalling iTerm2...\n'
+  brew cask install iterm2
 fi
 
